6e982e01590b4bb2529825920ec7aae42c6cd787
kjyothirani <katajyothi.rani@gmail.com>
2017-10-22 05:55:56 -0700

    Initial Commit

---
+++ src/main/java/com/ucmo/virtualclassroom/AcademicWiki.java
@@ -0,0 +1,11 @@
+package com.ucmo.virtualclassroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicWiki {
+	public static void main(String[] args) {
+		 SpringApplication.run(AcademicWiki.class, args);
+	}
+}
